ccc3059bddf4b812446e9b7c68c784c53e78bf43
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-04-28 12:25:47 +0200

    Render all movies properly. Showing pagination at the bottom only when request is completed.

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -2,29 +2,41 @@ import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 import Button from "react-bootstrap/Button"
 import swapi from "../services/swapi"
+import Loading from "../components/Loading"
+import Pagination from "../components/Pagination"
 
 const MoviesPage = () => {
+	const [data, setData] = useState([])
 	const [movies, setMovies] = useState([])
+	const [loading, setLoading] = useState(false)
+	const [page, setPage] = useState(1)
 
 	// Get todos from api
-	const getMovies = async () => {
-		const data = await swapi.getMovies()
-		setMovies(data.results)
+	const getMovies = async (page) => {
+		setLoading(true)
+
+		const res = await swapi.getMovies()
+		setData(res)
+		setMovies(res.results)
+
+		setLoading(false)
 	}
 
 	// Get todos from api when component is first mounted
 	useEffect(() => {
-		getMovies()
-	}, [])
+		getMovies(page)
+	}, [page])
 
 	return (
 		<>
 			<h1 className="mt-2">Movies</h1>
 
+			{loading && <Loading />}
+
 			{movies && (
 				<div className="people container d-flex flex-wrap gap-1">
 					{movies.map((movie) => (
-						<div className="card col-4" key={movie.name}>
+						<div className="card col-4" key={movie.title}>
 							<h2 className="card-header">{movie.title}</h2>
 							<div className="card-body">
 								<p>
@@ -37,7 +49,7 @@ const MoviesPage = () => {
 								<p>
 									{" "}
 									<span className="fw-bold">
-										Released: 
+										Released:
 									</span>{" "}
 									{movie.release_date}
 								</p>
@@ -48,13 +60,19 @@ const MoviesPage = () => {
 									</span>{" "}
 									{movie.characters.length}
 								</p>
-								<Button as={Link} to={`/films/${movie.episode_id}`}>
+								<Button
+									as={Link}
+									to={`/films/${movie.episode_id}`}
+								>
 									Read more
 								</Button>
 							</div>
 						</div>
 					))}
 				</div>
+			)}
+			{!loading && (
+				<Pagination page={page} setPage={setPage} data={data} />
 			)}
 		</>
 	)
