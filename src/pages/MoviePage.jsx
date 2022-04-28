986b4900793cb86ce7492d5ae3640f94915a546f
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-04-28 13:03:51 +0200

    Details about a movie fixed.

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -1,30 +1,97 @@
 import { useEffect, useState } from "react"
+import { useParams, Link, useNavigate } from "react-router-dom"
 import Button from "react-bootstrap/Button"
-import { useParams } from "react-router-dom"
+import Loading from "../components/Loading"
 import swapi from "../services/swapi"
-import { Link } from "react-router-dom"
 
 const MoviePage = () => {
-	const [movie, setMovie] = useState()
+	const [characters, setCharacters] = useState([])
+	const [error, setError] = useState(null)
+	const [loading, setLoading] = useState(false)
+	const [movieDetails, setMovieDetails] = useState([])
+	const navigate = useNavigate()
 	const { id } = useParams()
 
-	const getMovie = async (id) => {
-		const data = await swapi.getMovie(id)
-		setMovie(data)
+	const getMovie = async () => {
+		setLoading(true)
+
+		try {
+			const res = await swapi.getMovie(id)
+
+			setMovieDetails(res)
+			setCharacters(res.characters)
+			setLoading(false)
+		} catch (err) {
+			setLoading(false)
+			setError(err.message)
+		}
 	}
 
 	useEffect(() => {
-		getMovie(id)
-	}, [id])
-
-	if (!movie) {
-		return <p>Loading....</p>
-	}
+		getMovie()
+	}, [])
 
 	return (
-		<div>
-			<h3 className="mt-2"> {movie.title} </h3>
-		</div>
+		<>
+			{loading && <Loading />}
+
+			{error && { error }}
+
+			{movieDetails && (
+				<div className="movie container mt-5">
+					<div className="card col-12" key={movieDetails.title}>
+						<h2 className="card-header">{movieDetails.title}</h2>
+						<div className="card-body">
+							<h4>Attributes</h4>
+							<p>
+								{" "}
+								<span className="fw-bold">Episode:</span>{" "}
+								{movieDetails.episode_id}
+							</p>
+							<p>
+								<span className="fw-bold">Director: </span>
+								{movieDetails.director}
+							</p>
+							<p>
+								<span className="fw-bold">Producer: </span>
+								{movieDetails.producer}
+							</p>
+							<p>
+								{" "}
+								<span className="fw-bold">Released: </span>{" "}
+								{movieDetails.release_date}
+							</p>
+							<p>
+								<span className="fw-bold"> Characters: </span>
+							</p>
+							<ul className="list-group">
+								{characters.map((character) => (
+									<Link
+										key={swapi.getIdFromUrl(character)}
+										to={`/person/${swapi.getIdFromUrl(
+											character
+										)}`}
+									>
+										<li className="list-group-item">
+											Character{" "}
+											{swapi.getIdFromUrl(character)}
+										</li>
+									</Link>
+								))}
+							</ul>
+						</div>
+					</div>
+				</div>
+			)}
+			<Button
+				variant="primary"
+				className="mt-3 mb-5"
+				onClick={() => navigate(-1)}
+			>
+				{" "}
+				Go back{" "}
+			</Button>
+		</>
 	)
 }
 
